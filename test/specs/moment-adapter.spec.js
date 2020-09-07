b7b5a502eb6487bebcc5ac3a86de32c8f07fe6f8
stock <stocki.nail@gmail.com>
2020-09-07 21:52:07 +0200

    fixes lint errors

--- test/specs/moment-adapter.spec.js
+++ test/specs/moment-adapter.spec.js
@@ -11,7 +11,7 @@ describe('Moment Adapter', function() {
 		expect(adapter.format(1559056227321, formats.week)).toEqual('May 28, 2019');
 		expect(adapter.format(1559056227321, formats.day)).toEqual('May 28');
 	});
-	
+
 	it('should startOf correctly using isoWeek preset', function() {
 		const adapter = new _adapters._date();
 
@@ -19,50 +19,41 @@ describe('Moment Adapter', function() {
 		const daysInMonth = moment().daysInMonth();
 
 		for (const dayOfWeek of dayOfWeekNames) {
-			for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
-				const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
+			for (let dayOfMonth = 1; dayOfMonth <= daysInMonth; dayOfMonth++) {
+				const dt = moment({ day: dayOfMonth, hour: 8, minute: 30 });
 				const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', dayOfWeekNames.indexOf(dayOfWeek));
 				expect(adapter.format(startOf, 'ddd')).toEqual(dayOfWeek);
 				expect(startOf.day).not.toBeGreaterThan(dt.day);
 			}
 		}
 
-		for (const dayOfWeek of dayOfWeekNames) {
-			for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
-				const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
-				const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', false);
-				expect(adapter.format(startOf, 'ddd')).toEqual('Sun');
-				expect(startOf.day).not.toBeGreaterThan(dt.day);
-			}
+		for (let dayOfMonth = 1; dayOfMonth <= daysInMonth; dayOfMonth++) {
+			const dt = moment({ day: dayOfMonth, hour: 8, minute: 30 });
+			const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', false);
+			expect(adapter.format(startOf, 'ddd')).toEqual('Sun');
+			expect(startOf.day).not.toBeGreaterThan(dt.day);
 		}
 
-		for (const dayOfWeek of dayOfWeekNames) {
-			for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
-				const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
-				const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', true);
-				expect(adapter.format(startOf, 'ddd')).toEqual('Mon');
-				expect(startOf.day).not.toBeGreaterThan(dt.day);
-			}
+		for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
+			const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
+			const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', true);
+			expect(adapter.format(startOf, 'ddd')).toEqual('Mon');
+			expect(startOf.day).not.toBeGreaterThan(dt.day);
 		}
 
-		for (const dayOfWeek of dayOfWeekNames) {
-			for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
-				const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
-				const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', 100);
-				expect(adapter.format(startOf, 'ddd')).toEqual('Sat');
-				expect(startOf.day).not.toBeGreaterThan(dt.day);
-			}
+		for (let dayOfMonth = 1; dayOfMonth <= daysInMonth; dayOfMonth++) {
+			const dt = moment({ day: dayOfMonth, hour: 8, minute: 30 });
+			const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', 100);
+			expect(adapter.format(startOf, 'ddd')).toEqual('Sat');
+			expect(startOf.day).not.toBeGreaterThan(dt.day);
 		}
 
-		for (const dayOfWeek of dayOfWeekNames) {
-			for (let dayOfMonth=1; dayOfMonth<=daysInMonth; dayOfMonth++) {
-				const dt = moment({day: dayOfMonth, hour: 8, minute: 30});
-				const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', -100);
-				expect(adapter.format(startOf, 'ddd')).toEqual('Sun');
-				expect(startOf.day).not.toBeGreaterThan(dt.day);
-			}
+		for (let dayOfMonth = 1; dayOfMonth <= daysInMonth; dayOfMonth++) {
+			const dt = moment({ day: dayOfMonth, hour: 8, minute: 30 });
+			const startOf = adapter.startOf(dt.valueOf(), 'isoWeek', -100);
+			expect(adapter.format(startOf, 'ddd')).toEqual('Sun');
+			expect(startOf.day).not.toBeGreaterThan(dt.day);
 		}
 
-		
 	});
 });
