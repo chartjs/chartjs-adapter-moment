8f4fe401275725da688a1339b1d1e6d21eec7d5f
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-05-17 21:30:56 +0300

    Remove sourcemap (#34)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
- const resolve = require('@rollup/plugin-node-resolve').default;
- const terser = require('rollup-plugin-terser').terser;
- const pkg = require('./package.json');
- 
- const banner = `/*!
+const resolve = require('@rollup/plugin-node-resolve').default;
+const terser = require('rollup-plugin-terser').terser;
+const pkg = require('./package.json');
+
+const banner = `/*!
   * ${pkg.name} v${pkg.version}
   * ${pkg.homepage}
   * (c) ${new Date().getFullYear()} chartjs-adapter-moment Contributors
   * Released under the ${pkg.license} license
   */`;
- 
- const input = 'src/index.js';
- const external = [
-   'chart.js',
-   'moment'
- ];
- const globals = {
-   'chart.js': 'Chart',
-   moment: 'moment'
- };
+
+const input = 'src/index.js';
+const external = [
+  'chart.js',
+  'moment'
+];
+const globals = {
+  'chart.js': 'Chart',
+  moment: 'moment'
+};
 
 module.exports = [
   {
@@ -49,7 +49,6 @@ module.exports = [
       name: pkg.name,
       file: pkg.main.replace('.js', '.min.js'),
       format: 'umd',
-      sourcemap: true,
       indent: false,
       globals
     },
